8c7f032bc394915f55bb0fdac869e68fbf8e75ea
MStojak <mia.stojak29@gmail.com>
2022-10-08 16:51:42 +0200

    Add initial without .idea

---
+++ studiomoderna/studiomoderna/src/test/java/hr/studiomoderna/backend/studiomoderna/StudiomodernaApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.studiomoderna.backend.studiomoderna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudiomodernaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
